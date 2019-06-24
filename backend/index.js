656444d6132724aa276b3e9a5ab88c8538cc8c2f
Kevin Simper <kevin.simper@gmail.com>
2019-06-24 22:32:20 +0200

    Test the vision api

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-exports.checkFile = () => {};
+const vision = require("@google-cloud/vision");
+
+exports.checkFile = async () => {
+  async function quickstart() {
+    const client = new vision.ImageAnnotatorClient();
+    const [result] = await client.labelDetection("./apple.jpg");
+    const labels = result.labelAnnotations;
+    return result.labelAnnotations.map(({ description, score }) => ({
+      description,
+      score
+    }));
+  }
+  return await quickstart();
+};
--- backend/index.test.js
+++ backend/index.test.js
@@ -3,3 +3,8 @@ const { checkFile } = require("./index.js");
 test("checkFile to be defined", () => {
   expect(checkFile).toBeDefined();
 });
+
+test("checkFile should return apple", async () => {
+  const result = await checkFile();
+  expect(result.find(i => i.description === "Apple").description).toBe("Apple");
+});
