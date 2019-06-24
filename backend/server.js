6722d1f586ed0d2bdef1d0a8437bcc9ba2db6390
Kevin Simper <kevin.simper@gmail.com>
2019-06-24 23:06:32 +0200

    Call the vision api from the backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const http = require("http");
+const { checkFile } = require("./index.js");
+const { createVisionClient } = require("./visionClient.js");
 
-const server = http.createServer((req, res) => {
-  res.end("vegetables-goggles");
+const server = http.createServer(async (req, res) => {
+  console.log("New request", req.url);
+  if (req.url === "/checkfile") {
+    const visionClient = createVisionClient();
+    const result = await checkFile(visionClient);
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result));
+  } else {
+    res.end("vegetables-goggles");
+  }
 });
 
 exports.server = server;
